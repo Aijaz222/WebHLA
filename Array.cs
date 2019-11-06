1c1b2efc2fc2b6483cfb7ec77bed30553b4baff0
Aijaz222 <57089437+Aijaz222@users.noreply.github.com>
2019-11-06 23:22:55 +0500

    Array, Leapyear & SelectionSort Programs

---
+++ Array.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+
+class CopyCollectionToArray
+{
+
+    public static void Main()
+    {
+        ArrayList list = new ArrayList(5);
+        list.Add("BOB");
+        list.Add("TOM");
+        list.Add("Jerry");
+        list.Add("MOB");
+
+
+        object[] objectArray = list.ToArray();
+
+        string[] stringArray = new string[list.Count];
+        list.CopyTo(stringArray, 0);
+        string[] typedArray = (string[])list.ToArray(typeof(String));
+
+        Console.Write("Object array:");
+        foreach (string str in objectArray)
+        { Console.Write(" {0},", str); }
+        Console.Write("\nString array:");
+        foreach (string str in stringArray)
+        { Console.Write(" {0},", str); }
+        Console.Write("\nTyped  array:");
+        foreach (string str in typedArray)
+        { Console.Write(" {0},", str); }
+        Console.WriteLine();
+        Console.ReadLine();
+    }
+}
